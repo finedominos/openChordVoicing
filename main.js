2d8cbcd672d02463f9fe7151bc5017bee36e7c77
Aliette7878 <aliette.ravillion@yahoo.fr>
2019-12-06 16:32:33 +0100

    Sheet from codeopen

--- main.js
+++ main.js
@@ -39,8 +39,202 @@ new Vue({
             this.colorChosen = 'M';
             this.sevenChosen = 'm';
         },
-        go(){
-            alert("lets go with a sequence of "+this.chordSequence.length+" chords")
+        go() {
+            alert("lets go with a sequence of " + this.chordSequence.length + " chords")
         },
     }
 });
+
+
+//*************************************************************SHEET SECTION*************************************************************//
+const noteLength = 7 / 8
+const svgNS = 'http://www.w3.org/2000/svg'
+const xlinkNS = 'http://www.w3.org/1999/xlink'
+
+const ctrl = (function(ct) {
+        ct = [].slice.call(ct)
+        let ctrl = {}
+        for (const c of ct) ctrl[c.name] = c
+        return ctrl
+    })(document.querySelectorAll('.ctrl [name]'))
+    // const notes = "1234567+1;-(135)(246)(357)(461)(572)(613)(724)+(135)"
+const notes = '1[351],4565[247],2-7+12[136],11-7#+1-[7#25]+,-7#5'
+    // const rythm = "1-4444888811"
+const baseInterval = [2, 2, 1, 2, 2, 2, 1]
+const delay = 0.1
+console.log(ctrl)
+
+let interval
+let values
+
+function arrayRotate(arr, count) {
+    count = count % arr.length
+    if (count < 0) arr.unshift(...arr.splice(count))
+    else arr.push(...arr.splice(0, count))
+    return arr
+}
+
+ctrl.cmd.value = notes
+
+function mod(a, b) {
+    return a - b * Math.floor(a / b)
+}
+
+function parseNote(str) {
+    // str = str.replace(/\(([^)*|]*)\)/img, (...$) => `[${$[1]}]`)
+    let r
+    do {
+        r = 0
+        str = str.replace(/\(([^\(\)]*)\*([0-9]+)\)/gim, (...$) =>
+            $[1].repeat($[2], r++),
+        )
+    } while (r)
+    let octave = 4
+    const notes = []
+    let chord = null
+    let octaveGlob
+    let lastNote
+    for (let n of str) {
+        if (n === '+') octave++
+            else if (n === '-' && !chord) octave--
+                else if (n === '[') {
+                    chord = []
+                    octaveGlob = octave
+                    notes.push(chord)
+                } else if (n === ']') {
+            chord = null
+            lastNote = null
+            octave = octaveGlob
+        } else if (n === 'b') {
+            if (notes.length) {
+                const lastChord = notes[notes.length - 1]
+                const lastNote = lastChord[lastChord.length - 1]
+                lastNote[2] -= 1
+            }
+        } else if (n === '#') {
+            if (notes.length) {
+                const lastChord = notes[notes.length - 1]
+                const lastNote = lastChord[lastChord.length - 1]
+                lastNote[2] += 1
+            }
+        } else if (n === ',') {
+            notes.push([
+                []
+            ])
+        } else if (/[1-7]/.test(n)) {
+            n--
+            if (chord) {
+                if (lastNote && lastNote[0] >= n) octave++
+                    lastNote = [n, octave, 0]
+                chord.push(lastNote)
+            } else {
+                notes.push([
+                    [n, octave, 0]
+                ])
+            }
+        }
+    }
+    // console.log(JSON.stringify(notes,null,4))
+    return notes
+}
+
+const notesElem = document.getElementById('notes')
+
+function createNote(n, i) {
+    const note = document.createElementNS(svgNS, 'use')
+    let anchor
+    if (n.length === 0) anchor = '#double-rest'
+    else {
+        anchor = '#note'
+        if (n[2] > 0) anchor += '-sharp'
+        else if (n[2] < 0) anchor += '-flat'
+    }
+    note.setAttributeNS(xlinkNS, 'href', anchor)
+    note.setAttribute('x', i * 50)
+    if (n.length) note.setAttribute('y', n[0] * -10 - (n[1] - 4) * 10 * 7)
+    notesElem.appendChild(note)
+    n.elm = note
+    return note
+}
+let sheet = []
+
+function change() {
+    stop()
+    for (const c of sheet) {
+        for (const n of c) {
+            n.elm.remove()
+        }
+    }
+    let width = 0
+    sheet = parseNote(ctrl.cmd.value)
+    let i = 0
+    for (const c of sheet) {
+        for (const n of c) {
+            createNote(n, i)
+            width = Math.max(width, +n.elm.getAttribute('x'))
+        }
+        i++
+    }
+    document.querySelector('.svg>svg').style.width = `${width + 200}px`
+}
+ctrl.cmd.addEventListener('input', change)
+ctrl.cmd.addEventListener('keydown', e => {
+    console.log(e.which)
+    if (e.which == 13) return play()
+    if (e.which == 27) return stop()
+})
+change()
+
+function stop() {
+    for (const c of sheet) {
+        for (const n of c) {
+            clearTimeout(n.timeoutPlay)
+            clearTimeout(n.timeoutStop)
+            n.elm.classList.remove('playing')
+            n.osc && n.osc.disconnect()
+        }
+    }
+}
+
+function play() {
+    stop()
+    const ct = ac.currentTime
+
+    let i = 0
+    for (const c of sheet) {
+        const startTime = (i * 60) / tempo + delay
+        const stopTime = startTime + (60 * noteLength) / tempo
+        for (const n of c) {
+            if (n.length) {
+                const osc = ac.createOscillator()
+                osc.frequency.value =
+                    440 *
+                    Math.pow(2, (values[n[0]] + 3 + toneOffset + n[2]) / 12 + n[1] - 4)
+                osc.type = 'sawtooth'
+                osc.connect(volume)
+                osc.start(ct + startTime)
+                osc.stop(ct + stopTime)
+                n.osc = osc
+            }
+            n.timeoutPlay = setTimeout(() => {
+                n.elm.classList.add('playing')
+                if (
+                    n.elm.getBoundingClientRect().right - document.body.offsetWidth >
+                    0
+                ) {
+                    n.elm.scrollIntoView({
+                        behavior: 'smooth',
+                        block: 'start',
+                        inline: 'start',
+                    })
+                }
+            }, startTime * 1000)
+            n.timeoutStop = setTimeout(
+                () => n.elm.classList.remove('playing'),
+                stopTime * 1000,
+            )
+        }
+        i++
+    }
+}
+//************************************************************END OF SHEET SECTION*************************************************************//
