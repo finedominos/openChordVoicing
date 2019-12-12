65fe3e2e140ea0f9aa99f45c888a3b94250adc43
Aliette7878 <aliette.ravillion@yahoo.fr>
2019-12-12 15:21:19 +0100

    remove C_to_0, solving pb on sheet

--- sheet.js
+++ sheet.js
@@ -5,15 +5,15 @@ It has to call chord_list = output of chordAliette in chordrep.js*/
 const svgNS = 'http://www.w3.org/2000/svg'
 const xlinkNS = 'http://www.w3.org/1999/xlink'
 
-const list_notes = ["C", "D", "E", "F", "G", "A", "B"]
+//const list_notes = ["C", "D", "E", "F", "G", "A", "B"]
 
-function C_to_0(chord) {
+/*function C_to_0(chord) {
     var copy = JSON.parse(JSON.stringify(chord));
     for (var k = 0; k < chord.length; k++) {
         copy[k][0] = list_notes.indexOf(chord[k][0])
     }
     return copy
-}
+} */
 
 const ctrl = (function(ct) {
     ct = [].slice.call(ct)
@@ -34,6 +34,7 @@ function createElementofNote(n, i, id) {
     anchor = id
     note.setAttributeNS(xlinkNS, 'href', anchor)
     note.setAttribute('x', i * 150)
+    console.log(n)
     if (n.length) note.setAttribute('y', n[0] * -10 - (n[1] - 4) * 10 * 7)
     notesElem.appendChild(note)
 }
@@ -41,21 +42,23 @@ function createElementofNote(n, i, id) {
 const bar_m_needed = ['04', '55', '22']
 
 function createNote(n, i, shift) {
+    let j
+    j = i
     if (n[2] == 1) { createElementofNote(n, i, '#sharp') }
     if (n[2] == -1) { createElementofNote(n, i, '#flat') }
-    if (shift == "no_shift") { //then we add a going_up of down bar
-        if (n[1] > 5) { createElementofNote(n, i, '#going_down_bar') } else { createElementofNote(n, i, '#going_up_bar') }
-        createElementofNote(n, i, '#note')
-    } else { createElementofNote(n, i + 0.12, '#note') } //shift so no bar
+    if (shift == "shift") { j = i + 0.12 } else {}
+    //if (n[1] > 5) { createElementofNote(n, i, '#going_down_bar') } else { createElementofNote(n, i, '#going_up_bar') }
+    createElementofNote(n, j, '#going_up_bar')
+    createElementofNote(n, j, '#note')
     var a = n[0].toString() + n[1].toString()
-    if (bar_m_needed.includes(a)) { createElementofNote(n, i, '#bar_mid') }
+    if (bar_m_needed.includes(a)) { createElementofNote(n, j, '#bar_mid') }
 }
 
 
 function createChord(chord, i) {
     for (var k = 0; k < chord.length; k++) {
         if (k > 0) {
-            if ((chord[k][0] - chord[k - 1][0]) ** 2 < 2) {
+            if (chord[k][0] == (chord[k - 1][0] + 1)) {
                 createNote(chord[k], i, "shift")
             } else { createNote(chord[k], i, "no_shift") }
         } else { createNote(chord[k], i, "no_shift") }
@@ -65,23 +68,23 @@ function createChord(chord, i) {
 function createSheet(chord_list) {
     for (var i = 0; i < chord_list.length; i++) {
         createChord(chord_list[i], i)
-        console.log("done ")
     }
 }
 
+
 const chord_to_play1 = [
-    ['E', 2, 0],
-    ['C', 4, 0],
-    ['E', 4, 0],
-    ['F', 4, 1],
-    ['A', 5, 0]
+    [1, 2, 0],
+    [0, 4, 0],
+    [1, 4, 0],
+    [3, 4, 1],
+    [4, 5, 0]
 ]
 const chord_to_play2 = [
-    ['E', 4, -1],
-    ['B', 4, 1]
+    [6, 4, -1],
+    [5, 4, 1]
 ]
 
-createSheet([C_to_0(chord_to_play1), C_to_0(chord_to_play2)])
+createSheet([chord_to_play1, chord_to_play2])
 
 function cleanSheet() {
     var element = document.getElementById("notes");
