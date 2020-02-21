7f215efa4b04866d3da79f808c9cd3bc3c769c06
Aliette7878 <aliette.ravillion@yahoo.fr>
2020-02-22 00:11:17 +0100

    meeeh why is it so hard to play 2 notes

--- piano_notes/audio.js
+++ piano_notes/audio.js
@@ -1,33 +1,38 @@
 //***********************************AUDIO FUNCTION*****************************************//
 //Countains :
 // - sleep() to insert delay between playing the chords
-// - play_sheet() 
-// - all the variable linked to their audio file
+// - play_sheet() that is linked to the button
+// - audio files of the piano notes
 
 function sleep(delay) {
     var start = new Date().getTime();
     while (new Date().getTime() < start + delay);
 }
 
-test_sequence = [
-    [F4],
-    [G4, F4],
-    [G4]
-];
-
 function play_sheet(sheet) {
     for (var c in sheet) {
         chord = sheet[c];
         for (var n in chord) {
             note = chord[n];
-            note.currentTime = 0;
             note.play();
         }
-        sleep(3010);
+        sleep(3030);
+        for (var n in chord) {
+            note = chord[n];
+            note.pause();
+            note.currentTime = 0;
+        }
     }
 }
 
-audio_button.onclick = play_sheet;
+function play_testSequence() {
+    play_sheet([
+        [D4],
+        [F4, D4],
+    ])
+}
+
+audio_button.onclick = play_testSequence;
 
 //***************************BANK OF NOTES - 3 seconds each**************************//
 
