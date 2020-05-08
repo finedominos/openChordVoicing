8beb82d0a36a5ab9d4fd05dc2f7f1b315a5d00b4
andriana709 <andriana95@live.com>
2020-05-09 00:36:21 +0200

    Fixed clearing the canvas; added comments

--- keyboard.js
+++ keyboard.js
@@ -1,23 +1,26 @@
 var lastChordPrinted = null;
+
+//Initializing two canvases: one for the keyboard and another for the marked keys
 let canvas = document.getElementById("canvasKeyboard");
 let context = canvas.getContext("2d");
-canvas.width = keyboard.offsetWidth - 5;
-
 let canvas_keys = document.getElementById("canvasKeyboard");
 let context_keys = canvas_keys.getContext("2d");
+canvas.width = keyboard.offsetWidth - 5;
+canvas_keys.width = canvas.width;
 
+//Setting the keyboard image
 var img = new Image();
+
 img.onload = function () {
     context.drawImage(img, 0, 0, canvas.width, 240);
-    if(this.lastChordPrinted != null){
-        printChordOnKeyboard(lastChordPrinted) 
-    }
 }
+
+//Adjusting the size of the keyboard image and the marked keys according to the window size
 window.onresize = function () {
     context.clearRect(0, 0, canvas.width, canvas.height);
     canvas.width = 0.782 * window.innerWidth;
     canvas_keys.width = canvas.width;
-    // console.log("canva's new width : "+canvas.width);
+    //Each time the window gets resized, the keys of the last chord get marked again
     if(this.lastChordPrinted != null){
         printChordOnKeyboard(lastChordPrinted) 
     }
@@ -25,15 +28,17 @@ window.onresize = function () {
 
 img.src = "pianoKeyboard88keys.png";
 
+
+//Marking the chord notes on the keyboard
 function printChordOnKeyboard(chord) {
+
 
     var keywidth_white = canvas.width / 52;
     var keywidth_black = keywidth_white / 1.6;
-
     let whiteKeys = [0, 1, 3, 5, 6, 8, 10];
 
-    //context_keys.clearRect(0, 140, canvas.width, 10);
-
+    //Clearing the canvas and reloading the keyboard image
+    context.clearRect(0, 0, canvas.width, canvas.height);
     context.drawImage(img, 0, 0, canvas.width, 240);
 
     chord.forEach(key => {
@@ -41,12 +46,12 @@ function printChordOnKeyboard(chord) {
         x = keywidth_white;
         x += Math.floor(key / 12) * keywidth_white * 7;
         keyInOctave = key % 12;
-        
-        //console.log("key", key);
-        //console.log("keyInOctave", keyInOctave);
 
         context_keys.beginPath();
 
+        //Marking keys to be played with a combination of red rectangles on the keyboard
+
+        //If a key to be marked is a white key
         if (whiteKeys.includes(keyInOctave)){
             keywidth = keywidth_white;
             y = 160;
@@ -59,24 +64,27 @@ function printChordOnKeyboard(chord) {
             else {
                 x += 3 * keywidth + (keyInOctave - 4) * keywidth * 0.5;
             }
+
+            //Drawing the upper rectangle of a white key
+            //Keys C and F
             if (keyInOctave == 1 || keyInOctave == 6){
-                //C and F
                 context_keys.rect(x, 0, keywidth_white * 0.53, 160);
             }
+            //Keys D and G
             else if (keyInOctave == 3 || keyInOctave == 8){
-                //D and G
                 context_keys.rect(x + keywidth_white * 0.21, 0, keywidth_white * 0.56, 160);
             }
+            //Keys E and B    
             else if (keyInOctave == 5 || keyInOctave == 0){
-                //E and B
                 context_keys.rect(x + keywidth_white * 0.51, 0, keywidth_white * 0.49, 160);
             }
+            //Key A
             else {
-                //A
                 context_keys.rect(x + keywidth_white * 0.36, 0, keywidth_white * 0.45, 160);
             }
             
         }
+        //If a key to be marked is a black key
         else {
             keywidth = keywidth_black;
             y = 0;
@@ -94,10 +102,9 @@ function printChordOnKeyboard(chord) {
         context_keys.fillStyle = "red";
         context_keys.fill();
     });
+    //Remembering the last chord
     lastChordPrinted = chord;
-
 }
-
 
 
 
